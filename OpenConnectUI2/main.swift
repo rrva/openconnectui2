ab2db9735791aba43615a9d45166dcd500ec9141
Ragnar Rova <ragnar.rova@svt.se>
2022-03-27 15:38:02 +0200

    Show alert when running from a read-only volume

--- OpenConnectUI2/main.swift
+++ OpenConnectUI2/main.swift
@@ -7,9 +7,18 @@ func main() {
 
   let app = NSApplication.shared
 
+  do {
+    let resourceValues = try Bundle.main.bundleURL.resourceValues(forKeys: [.volumeIsReadOnlyKey])
+    if (resourceValues.volumeIsReadOnly ?? false) == true {
+      modalDialog(messageText: "Read-only app location", informativeText: "Please move this app to the Applications folder and open it from there")
+      app.terminate(nil)
+    }
+  } catch {
+    print(error)
+  }
+
   let authorized = DispatchSemaphore(value: 0)
   let installed = DispatchSemaphore(value: 0)
-
   checkHelperInstallation { isInstalled in
     if isInstalled == false {
       logger.log("Installing helper")
@@ -44,4 +53,13 @@ extension CommandLine {
     }
     return false
   }
+}
+
+func modalDialog(messageText: String, informativeText: String) {
+    let alert = NSAlert()
+    alert.messageText = messageText
+    alert.informativeText = informativeText
+    alert.alertStyle = .warning
+    alert.addButton(withTitle: "OK")
+    alert.runModal()
 }
