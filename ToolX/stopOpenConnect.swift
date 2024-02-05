bedf8e6bb1e2ece87c6f832cf059d5ee63ab63d0
Ragnar Rova <ragnar.rova@gmail.com>
2024-02-05 09:01:17 +0100

    More graceful shutdown allowing openconnect to run vpnc

--- ToolX/stopOpenConnect.swift
+++ ToolX/stopOpenConnect.swift
@@ -3,7 +3,7 @@ import Foundation
 func doStopOpenConnect(_ reply: (String) -> Void) {
   do {
     let pipe = Pipe()
-    let task = try safeShell("pkill openconnect", pipe: pipe)
+    let task = try safeShell("pkill -INT openconnect", pipe: pipe)
     pipe.fileHandleForReading.readabilityHandler = { pipe in
       if let line = String(data: pipe.availableData, encoding: String.Encoding.utf8) {
         NSLog(line)
